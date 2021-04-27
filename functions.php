34259a583913a21a13572086904c9f4b1084efd8
unknown <amjadbashir.ui7@gmail.com>
2021-04-27 04:36:06 -0700

    Header Right Option added

--- functions.php
+++ functions.php
@@ -117,26 +117,6 @@ function roon_content_width() {
 add_action( 'after_setup_theme', 'roon_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function roon_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'roon' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'roon' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'roon_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function roon_scripts() {
@@ -182,3 +162,13 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  * Theme option additions.
  */
 require get_template_directory() . '/inc/themeoption.php';
+
+/**
+ * Theme helper additions.
+ */
+require get_template_directory() . '/inc/helperfunction.php';
+
+/**
+ * Theme widget additions.
+ */
+require get_template_directory() . '/inc/widgetarea.php';
